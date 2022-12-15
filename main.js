714c23f6d6b0ee895f28cf21199a8e6c557b5a58
MeesterMarcus <lorenzanamarcus@gmail.com>
2022-12-14 18:14:33 -0600

    beaver builder launcher

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    console.log(tabs)
-    
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-    console.log(url)
-    document.getElementById("btn").addEventListener("click", () => {
-        const hash = document.getElementById("hash").value
-        console.log(hash)
-        document.write(`${url}/commit/${hash}`)
-    });
-    
-});
+const url = document.URL
+const flUrl = `${url}/?fl_builder`
+window.location.href = flUrl;
