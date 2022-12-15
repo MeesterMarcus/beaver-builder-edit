0d46e90937fee5838f884c08268f7f1588615e57
MeesterMarcus <lorenzanamarcus@gmail.com>
2022-12-14 18:32:26 -0600

    formatting

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['main.js']
-    });
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['main.js']
   });
+});
